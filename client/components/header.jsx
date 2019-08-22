11ce796a000b6fb0b28581142ab153d19c4768dd
johndedios94 <johndedios94@gmail.com>
2019-08-22 21:29:40 +0000

    Finished view product front end

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div>
+      <div className='headerbg'>
         <h1 className='header'>Wicked Sales</h1>
         <div className='cart'></div>
       </div>
