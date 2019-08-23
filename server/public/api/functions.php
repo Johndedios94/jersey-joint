99fa4f8d098d028882c17f011de9d31fd3a5064f
johndedios94 <johndedios94@gmail.com>
2019-08-23 17:49:35 +0000

    Completed connect to products table and fetch data

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,13 +1,13 @@
-<?php
 
-function error_handling($error){
+<?php
+ function error_handling($error){
   $output = ["success" =>false,
             "error:" => $error -> getMessage()];
-  http_response_code(500);
   $json_output= json_encode($output);
   print($json_output);
 
-}
-
-
-?>
+  function startup(){
+    return header('Content-Type: application/json');
+  }
+ }
+ ?>
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,10 +1,25 @@
 <?php
-header('Content-Type: application/json');
 require_once('functions.php');
 set_exception_handler('error_handling');
-
+startup();
 require_once('db_connection.php');
-$output = file_get_contents('dummy-products-list.json');
-print($output);
+
+$query = "SELECT * FROM `products` WHERE `price` < 2000";
+ $result = $conn->query($query);
+ $output=[];
+if($result = mysqli_query($conn, $query)){
+  $rows = mysqli_num_rows($result);
+  if($rows === 0 ){
+    print('no data available');
+    exit();
+  }
+  while($row = mysqli_fetch_assoc($result)){
+    $output[] = $row;
+  }
+ } else {
+  throw new Exception("Error message: %s\n", mysqli_error($conn));
+}
+ print(json_encode($output))
+
 
 ?>
