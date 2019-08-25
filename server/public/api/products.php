b3a23d33f59f49e8b8857ea83b549b5f74205497
johndedios94 <johndedios94@gmail.com>
2019-08-25 02:09:57 +0000

    Completed up to checking if id is a number

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,10 +1,23 @@
 <?php
 require_once('functions.php');
 set_exception_handler('error_handling');
-startup();
+// startup();
 require_once('db_connection.php');
 
-$query = "SELECT * FROM `products` WHERE `price` < 2000";
+
+if(empty($_GET['id'])){
+   $whereClause = " ";
+  //  $whereClause = "WHERE `id` < 5";
+} else if (!is_numeric($_GET['id'])) {
+     throw new Exception('id not valid');
+} else{
+   $whereClause = "WHERE `id` = " . $_GET['id'];
+};
+  //  $whereClause = "WHERE `id` < 5";
+
+
+$query = "SELECT * FROM `products`" . $whereClause;
+
  $result = $conn->query($query);
  $output=[];
 if($result = mysqli_query($conn, $query)){
