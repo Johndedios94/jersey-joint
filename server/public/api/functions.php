105db317594ec4c57a25e40080a60746c3fa9b20
johndedios94 <johndedios94@gmail.com>
2019-08-26 19:49:11 +0000

    Finished adapt product details

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,13 +1,12 @@
 
 <?php
+  function startup(){
+    return header('Content-Type: application/json');
+  }
  function error_handling($error){
   $output = ["success" =>false,
             "error:" => $error -> getMessage()];
   $json_output= json_encode($output);
   print($json_output);
-
-  function startup(){
-    return header('Content-Type: application/json');
-  }
  }
  ?>
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,38 +1,33 @@
 <?php
 require_once('functions.php');
 set_exception_handler('error_handling');
-// startup();
+startup();
 require_once('db_connection.php');
 
 
 if(empty($_GET['id'])){
    $whereClause = " ";
-  //  $whereClause = "WHERE `id` < 5";
-} else if (!is_numeric($_GET['id'])) {
-     throw new Exception('id not valid');
-} else{
+}
+if(!empty($_GET['id'])){
    $whereClause = "WHERE `id` = " . $_GET['id'];
 };
-  //  $whereClause = "WHERE `id` < 5";
+if(!empty($_GET['id']) && !is_numeric($_GET['id']) ) {
+     throw new Exception('id needs to be a number');
+}
 
 
 $query = "SELECT * FROM `products`" . $whereClause;
-
- $result = $conn->query($query);
- $output=[];
-if($result = mysqli_query($conn, $query)){
-  $rows = mysqli_num_rows($result);
-  if($rows === 0 ){
-    print('no data available');
-    exit();
-  }
+$result = $conn->query($query);
+if(mysqli_num_rows($result) === 0 && !empty($_GET['id'])){
+  throw new Exception("Invalid ID: " . $_GET['id']);
+}
+if(!$result){
+  throw new Exception(mysqli_connect_error($conn));
+};
+  $output = array();
   while($row = mysqli_fetch_assoc($result)){
-    $output[] = $row;
-  }
- } else {
-  throw new Exception("Error message: %s\n", mysqli_error($conn));
+  $output[] = $row;
 }
  print(json_encode($output))
-
 
 ?>
